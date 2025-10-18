33d408ed7b52ea688bef489dad9396ff2f1ad3f6
Sopeade <lanlehinsope@gmail.com>
2025-10-18 15:59:59 -0500

    basic mobile version done

--- product-list-with-cart-main/app.js
+++ product-list-with-cart-main/app.js
@@ -3,15 +3,17 @@ const { createApp, ref, onMounted } = Vue
 createApp({
   setup() {
     const data = ref('')
+    const my_name = ref('')
     onMounted(async()=>{
       const res = await fetch('./data.json')
       data.value = await res.json()
     })
     const message = ref('Successfully Imported Data using CDN!')
-    
+    console.log("my_name", my_name.value)
     return {
       message,
       data,
+      my_name,
     }
   }
 }).mount('#app')
