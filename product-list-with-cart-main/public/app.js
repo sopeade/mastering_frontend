5529e980fb64bf9ef74f869926b7f38448fdbede
sopeade <lanlehinsope@gmail.com>
2026-01-03 21:30:18 -0600

    added list transition

--- product-list-with-cart-main/public/app.js
+++ product-list-with-cart-main/public/app.js
@@ -4,41 +4,57 @@ const { createApp, ref, onMounted, onUpdated, watch, computed } = Vue;
 createApp({
     setup(){
         const data = ref('');
-        const cart = ref({
+        const emptyCart = {
             quantity: [],
             price: [],
             itemTotal: [],
             cartTotal: 0,
             totalNumItems: 0,
             itemIndexes: [],
-        })
-        const modal = ref(false);
+        }
+        const cart = ref({...emptyCart})
+        const isCheckedOut = ref(false);
 
         cart.value.itemTotal = computed(()=> {
+            console.log("computed itemtotal")
             return cart.value.price.map((val, idx) => val * cart.value.quantity[idx]);
         })
 
         cart.value.cartTotal = computed(() => {
+            console.log("computed carttotal")
             let totalSum= 0;
             cart.value.itemTotal.forEach(num => totalSum += num)
            return totalSum.toFixed(2);
         })
 
         cart.value.totalNumItems = computed(() => {
+           console.log('totalnumitems')
             let sum  = 0;
             cart.value.quantity.forEach(num => sum += num)
             return sum;
         })
-        // cart.value.totalNumItems = 2
+
+        // cart.value.itemIndexes = computed(() => {
+        //     cart.value.itemIndexes = [];
+        //     console.log("yo cart.value.itemIndexes:", cart.value.itemIndexes, cart)
+        //     cart.value.quantity.forEach((ele, idx) => {
+        //         if (ele > 0){
+        //             cart.value.itemIndexes.push(idx);
+        //         }
+        //     })
+        // })
 
         watch(cart.value.quantity, () =>{
+        // watch(cart, () =>{
+            console.log("watch quantity triggered")
             cart.value.itemIndexes = [];
             cart.value.quantity.forEach((ele, idx) => {
                 if (ele > 0){
                     cart.value.itemIndexes.push(idx);
                 }
             })
-        })
+        } )
+
 
         onMounted(async()=> {
             const res = await fetch('./data.json');
@@ -49,18 +65,34 @@ createApp({
             })
         })
 
-        onUpdated(() => {
-            console.log("cart", cart)
-        })
-        // cart.value.quantity = [ 0, 2, 3, 0, 0, 1, 0, 0];
+
+        const resetCart = () => {
+            console.log("b4resetCart", cart.value)
+            Object.assign(cart.value, emptyCart);
+            // cart.value = {...emptyCart} # this breaks reactivity by creating a new object reference
+            console.log("after resetCart", cart.value)
+            isCheckedOut.value = false;
+            data.value.forEach((item, idx) => {
+                cart.value.quantity[idx] = 0
+            })
+            // cart.value.quantity = Array(data.value.length).fill(0); # this breaks reactivity
+            console.log("cart.value final is:", cart.value)
+        }
+
+        const closeModal = () => {
+            isCheckedOut.value = false;
+            console.log("closeModal", isCheckedOut.value)
+        }
 
         return {
+            isCheckedOut,
             cart,
+            closeModal,
             data,
-            increment,
             decrement,
-            modal,
+            increment,
             removeItem,
+            resetCart,
         }
     }
 }).mount("#app");
--- product-list-with-cart-main/public/utils.js
+++ product-list-with-cart-main/public/utils.js
@@ -2,22 +2,22 @@
 export const increment = (cart, idx)=> {
     console.log("increment")
    cart.quantity[idx] += 1;
-    console.log("cart.quantity", cart.quantity[idx])
+    console.log("cart", cart)
 }
 
 export const decrement = (cart, idx)=> {
-    console.log("decrement before", cart.quantity[idx])
+    console.log("decrement before", cart)
    const currVal = cart.quantity[idx]
       if (currVal <= 0){
         cart.quantity[idx] = 0;
         return;
       }
    cart.quantity[idx] -= 1;
-    console.log("decrement after", cart.quantity[idx])
+    console.log("decrement after", cart)
 }
 
 export const removeItem = (cart, idx)=> {
-    console.log("removeItem")
+    console.log("removeItem", idx)
     cart.quantity[idx] = 0;
 }
 
